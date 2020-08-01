11166f066ff4b11ec0ae843ca2d85fbdeb125943
Mauricio Cardozo <loloop@live.com>
2020-08-01 02:49:34 -0300

    Initial Commit

---
+++ SwiftInMotion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftInMotion
+//
+//  Created by Mauricio Cardozo on 8/1/20.
+//  Copyright © 2020 Mauricio Cardozo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
